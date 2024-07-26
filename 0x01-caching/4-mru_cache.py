a29f0546ac6744b9ffe764eb6adc1ded91c1ec8f
SohailPro12 <131702341+SohailPro12@users.noreply.github.com>
2024-07-26 16:20:39 +0200

    task 4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" MRUCache module
+"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class MRUCache(BaseCaching):
+    """
+    MRUCache function
+    """
+    def __init__(self):
+        """
+        constructor of the func
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+        put function
+        """
+
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            if key not in self.cache_data:
+                key_discarded, _ = self.cache_data.popitem(False)
+                print("DISCARD:", key_discarded)
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=False)
+
+    def get(self, key):
+        """
+        get function
+        """
+        if key is None:
+            return
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key)
