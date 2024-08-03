2508ff20f967fa31a0e7ef04486e4a469d14223e
Sohail Charef <charefsohayl@gmail.com>
2024-08-03 10:18:29 +0000

    task1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -2,16 +2,30 @@
 """
 Basic Babel setup
 """
+
 from flask import Flask, render_template
 from flask_babel import Babel
 
+
+class Config:
+    '''Config class'''
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
 app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+
+babel = Babel(app)
 
 
 @app.route('/')
 def index():
-    """Main page"""
-    return render_template("0-index.html")
+    """default route"""
+    return render_template("1-index.html",)
 
 
 if __name__ == "__main__":
